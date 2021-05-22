896455660dd2b8dc1d1d0db8736b176184993fed
Joash Nyabuti <nyabutijoash@yahoo.com>
2021-05-22 12:52:02 -0500

    configured url sting

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const url=process.env.DATA_STRING;
 console.log(url);
- mongoose.connect("mongodb+srv://shoppy123:shoppy123@cluster0.hi6if.mongodb.net/sample_analytics?retryWrites=true&w=majority", {
+ mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
